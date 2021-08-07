48c198b6687eaea243a00444192c6c9a94eb40b3
maftuna99 <rajabovamaftuna10@gmail.com>
2021-08-06 18:11:25 -0700

    Test case is all done

--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -4,4 +4,8 @@ public class ResetPractice {
     // part 1 is done
     // part 2 is done
     // part 3 is done
+    // part 4 is done
+    // part 5 is done
+    // Mixed | Soft reset remove the commit from history but keep the local change
+    // Did part 4 part 5 and I am done in one commit
 }
